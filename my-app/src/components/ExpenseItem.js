f0f14bbdc25ccdf25751fdc08a09ee8d1779c403
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-01 09:05:33 +0100

    code added

--- my-app/src/components/ExpenseItem.js
+++ my-app/src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 import React from 'react';
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
-function ExpenseItem() {
-	const expenseDate = new Date(2022, 11, 1);
-	const expenseTitle = 'Car Insurance';
-	const expenseAmount = 299.99;
-
-	return (
-  <div className='expense-item'>
-	<div>{expenseDate}</div>
-		<div className='expense-item__description'>
-			<h2>{expenseTitle}</h2>
-			<div className='expense-item__price'>${expenseAmount}</div>
-		</div>
-	</div>
-	
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
   );
 }
 
