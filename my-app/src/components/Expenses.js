bf7c5cda27f1cf0837681e506798bbf68a9b9bdd
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-12-01 10:07:45 +0100

    name changed

--- my-app/src/components/Expenses.js
+++ my-app/src/components/Expenses.js
@@ -5,24 +5,24 @@ function Expenses(props) {
 	return (
 		<div>
 			<ExpenseItem
-				title={props.expenses[0].title}
-				amount={props.expenses[0].amount}
-				date={props.expenses[0].date}
+				title={props.item[0].title}
+				amount={props.item[0].amount}
+				date={props.item[0].date}
 			/>
 			<ExpenseItem
-				title={props.expenses[1].title}
-				amount={props.expenses[1].amount}
-				date={props.expenses[1].date}
+				title={props.item[1].title}
+				amount={props.item[1].amount}
+				date={props.item[1].date}
 			/>
 			<ExpenseItem
-				title={props.expenses[2].title}
-				amount={props.expenses[2].amount}
-				date={props.expenses[2].date}
+				title={props.item[2].title}
+				amount={props.item[2].amount}
+				date={props.item[2].date}
 			/>
 			<ExpenseItem
-				title={props.expenses[3].title}
-				amount={props.expenses[3].amount}
-				date={props.expenses[3].date}
+				title={props.item[3].title}
+				amount={props.item[3].amount}
+				date={props.item[3].date}
 			/>
 		</div>
 	);
